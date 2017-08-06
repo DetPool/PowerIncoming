795c4325842f6cc7c4172989cb88c3cc3532fb11
DetPool <detlef.fehr666@gmail.com>
2017-08-06 12:44:08 +0200

    Initial commit

---
+++ config.lua
@@ -0,0 +1,6 @@
+--------------------------------------
+-- Namespaces
+--------------------------------------
+local _, core = ...
+core.Config = {} -- adds Config table to addon namespace
+local Config = core.Config
---
+++ lib.lua
@@ -0,0 +1,6 @@
+--------------------------------------
+-- Namespaces
+--------------------------------------
+local _, core = ...
+core.Lib = {} -- adds Lib table to addon namespace
+local Lib = core.Lib
